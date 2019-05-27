b433df57dac957403201e6c3bc0afd86518a39e1
Shin-Higemaru <shijinboshi@hotmail.fr>
2019-05-27 16:16:59 +0200

    newc

--- app/Adresse.php
+++ app/Adresse.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Adresse extends Model
 {
     // Déclaration des champs qui pourront être remplis via la validation d'un formulaire
-    protected $fillable = ['prenom','nom','adresse','adresse2','code_postal','ville','pays'];
+    protected $fillable = ['prenom','nom','adresse','adresse2','telephone','code_postal','ville','pays'];
 }
